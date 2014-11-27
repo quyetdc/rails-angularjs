e02f61390203b273e3474bbc4607f52eea850704
quyetdc <quyetdc.uet@gmail.com>
2014-11-27 15:56:39 +0700

    small fix on dashboard tag

--- app/controllers/angular/dashboard_controller.rb
+++ app/controllers/angular/dashboard_controller.rb
@@ -3,6 +3,13 @@ module Angular
     def index
       ## todo: change @user here
       @user = current_user
+      @snippets = @user.snippets
+      _tags = []
+      @snippets.each do |snippet|
+        _tags = _tags | snippet.tags
+      end
+
+      @tags = _tags
     end
   end
 end

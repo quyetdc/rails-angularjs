6d381f9e759bab254425f3c85389c44901330734
quyetdc <quyetdc.uet@gmail.com>
2014-12-18 10:19:30 +0700

    generate Friendship model and controller

---
+++ db/migrate/20141218021058_create_friendships.rb
@@ -0,0 +1,14 @@
+class CreateFriendships < ActiveRecord::Migration
+  def change
+    create_table :friendships do |t|
+      t.string :status
+      t.integer :user_id
+      t.integer :friend_id
+
+      t.timestamps
+    end
+
+    add_index :friendships, :user_id
+    add_index :friendships, :friend_id
+  end
+end
